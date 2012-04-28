eb4cdbccf6d086454eff20147dc562eecd05f617
Krishna Kamath <krishna.kamath@gmail.com>
2012-04-28 09:30:24 -0500

    modified memcache

--- src/helloworld.py
+++ src/helloworld.py
@@ -3,21 +3,23 @@ import datetime
 import urllib
 import wsgiref.handlers
 import os
+from django.utils import simplejson as json
 from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
-import json
-
 from google.appengine.api import memcache
+
 class MemecacheObject(object):
     def load(self, json_object):
         self.__dict__ = json.loads(json_object)
+        return self
     def dump(self):
         return json.dumps(self.__dict__)
     def printo(self):
         print self.__dict__
+
 class Person(MemecacheObject):
     def __init__(self):
         self.name = None
@@ -29,9 +31,12 @@ class Person(MemecacheObject):
 
 class MainPage(webapp.RequestHandler):
     def get(self):
+        memcache_client = memcache.Client()
         self.response.out.write('<html><body>')
-        persons = db.GqlQuery("SELECT * FROM Person")
-        
+#        persons = db.GqlQuery("SELECT * FROM Person")
+        persons = []
+        json_person = memcache_client.get('current_person')
+        if json_person: persons.append(Person().load(memcache_client.get('current_person')))
         template_values = {
             'persons': persons,
         }
@@ -58,7 +63,9 @@ class Guestbook(webapp.RequestHandler):
         person = Person()
         person.name = self.request.get('name')
         person.age = int(self.request.get('age'))
-        person.put()
+#        person.put()
+        memcache_client = memcache.Client()
+        memcache_client.set(key="current_person", value=person.dump(), time=3600)
         self.redirect('/')
 
 
@@ -73,8 +80,8 @@ def main():
 
 
 if __name__ == '__main__':
-#    main()
-    p = Person()
-    p.name = 'ssd'
-    p.age = 12
-    print p.dump()
+    main()
+#    p = Person().load('{"age": 12, "name": "ssd"}')
+##    p.name = 'ssd'
+##    p.age = 12
+#    p.printo()
