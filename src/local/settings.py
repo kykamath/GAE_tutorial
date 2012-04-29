50dfe05e6f6c465ad4ccdc0525c0e3dbe08bca45
Krishna Kamath <krishna.kamath@gmail.com>
2012-04-29 15:39:59 -0500

    added stream parser

---
+++ src/local/settings.py
@@ -0,0 +1,8 @@
+'''
+Created on Apr 29, 2012
+
+@author: kykamath
+'''
+
+#f_hashtags_geo_distribution = '/mnt/chevron/kykamath/data/geo/hashtags_geo_distribution/%s/%s/%s/%s_%s'
+f_hashtags_geo_distribution = '/data/chevron/kykamath/data/geo/hashtags_geo_distribution/%s/%s/%s/%s_%s'
---
+++ src/local/twitter_stream_parser.py
@@ -0,0 +1,48 @@
+'''
+Created on Apr 29, 2012
+
+@author: kykamath
+'''
+import tweetstream
+import cjson
+from settings import f_hashtags_geo_distribution
+from library.geo import getCenterOfMass
+from library.twitter import getDateTimeObjectFromTweetTimestamp
+from library.file_io import FileIO
+
+USER_NAME = 'worldw_crawler'
+PASSWORD = 'krishna'
+LOCATIONS = ['-180.0,-90.0', '180.0,90.0']
+
+def ParseGeoData(data):
+    if 'geo' in data and data['geo']!=None: return ('geo', data['geo']['coordinates'])
+    elif 'place' in data: 
+        point = getCenterOfMass(data['place']['bounding_box']['coordinates'][0])
+        return ('bb', [point[1], point[0]])
+def ParseHashtags(tweet):
+    hashtags = []
+    for h in tweet['entities']['hashtags']: hashtags.append(h['text'])
+    return hashtags
+def GetCheckinObject(data):
+    checkin = {'user': {'id': data['user']['id'], 'l': data['user']['location']}, 'id': data['id'], 't': data['created_at'], 'h': [], 'tx': data['text']}
+    return checkin
+def GetOutputFile(tweet):
+    t = getDateTimeObjectFromTweetTimestamp(tweet['created_at'])
+    return f_hashtags_geo_distribution%(t.year, t.month, t.day, t.hour, t.minute)
+
+def parse_stream():    
+    stream = tweetstream.FilterStream(USER_NAME, PASSWORD, locations=LOCATIONS) 
+    for tweet in stream:
+        try:
+            geo = ParseGeoData(tweet)
+            if geo: 
+                hashtags = ParseHashtags(tweet)
+                if hashtags: 
+                    checkin_object = GetCheckinObject(tweet)
+                    checkin_object['h'] = hashtags
+                    checkin_object[geo[0]] = geo[1]
+                    FileIO.writeToFileAsJson(checkin_object, GetOutputFile(tweet))
+        except Exception as e: print e
+
+if __name__ == '__main__':
+    parse_stream()
