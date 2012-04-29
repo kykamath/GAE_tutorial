f2ff2bb0cf0232ffd1a1fb683781de8cb542473d
Krishna Kamath <krishna.kamath@gmail.com>
2012-04-29 01:12:49 -0500

    modified js

---
+++ src/js/map.js
@@ -0,0 +1,27 @@
+var markers = [
+				'37.509726,-113.291016',
+				'45.089036,-102.041016',
+				'33.358062,-91.230469',
+				'40.713956,-76.025391'
+			]
+
+$(document).ready(function(){
+	// $("p").click(function() {
+		// $(this).hide(300);
+	// });
+	
+// 	Init tabs
+	$('#tabs').tabs();
+	
+// 	Init map
+	var yourStartLatLng = '59.3426606750, 18.0736160278';
+    $('#map_canvas').gmap().bind('init', function(event, map) {
+     	$.each(markers, function(index, marker) {
+		   $('#map_canvas')
+     			.gmap(
+	     			'addMarker', 
+	     			{'position': marker, 'bounds': true, 'icon': '/images/blue_signal.png'}
+     			)
+		 });
+    });
+});
