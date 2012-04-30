e31d4506608116183066a6d594abb32fca7610c1
Krishna Kamath <krishna.kamath@gmail.com>
2012-04-30 10:39:45 -0500

    modified js

--- src/remote/js/map.js
+++ src/remote/js/map.js
@@ -1,6 +1,6 @@
 var locations = null;
 
-function PlotOnMap(hashtag_id) {
+function PlotGlobalSpreadOnMap(hashtag_id) {
 	locations_for_hashtag = locations[hashtag_id];
 	$('#map_canvas').gmap('clear', 'markers');
 	var mark_clusterter = $('#map_canvas').gmap('get', 'MarkerClusterer');
@@ -18,6 +18,40 @@ function PlotOnMap(hashtag_id) {
 	$('#map_canvas').gmap('set', 'MarkerClusterer', new MarkerClusterer($('#map_canvas').gmap('get', 'map'), $('#map_canvas').gmap('get', 'markers')));
 }
 
+function GetLatLngFromLatLngStr(str_lat_lon) {
+	var result_of_split = str_lat_lon.split(',');
+	result_of_split[0] = parseFloat(result_of_split[0]);
+	result_of_split[1] = parseFloat(result_of_split[1]);
+	return result_of_split
+}
+
+function PlotSpreadPathOnMap(hashtag_id) {
+	path_for_hashtag = ['37.509726,-113.291016', '45.089036,-102.041016', '33.358062,-91.230469', '40.713956,-76.025391']
+	var iteration_counter = 0;
+	$.each(path_for_hashtag, function(index, location) {
+		var co_ordinates = GetLatLngFromLatLngStr(location);
+
+		setTimeout(function() {
+			$('#map_path').gmap3({
+				action : 'addMarker',
+				latLng : [co_ordinates[0], co_ordinates[1]],
+				options : {
+					// draggable : true,
+					animation : google.maps.Animation.DROP
+				},
+				callback : function(marker) {
+					$('#map_path').gmap3({
+						action : 'panTo',
+						args : [marker.position]
+					});
+				},
+			});
+		}, iteration_counter * 2000); 
+
+
+		iteration_counter += 1
+	});
+}
 
 $(document).ready(function() {
 	// Init dropdown menu
@@ -26,7 +60,7 @@ $(document).ready(function() {
 		style : 'dropdown'
 	}).change(function() {
 		// $('select#hashtags').text('sada');
-		PlotOnMap(this.value);
+		PlotGlobalSpreadOnMap(this.value);
 	});
 	// 	Init tabs
 	$('#tabs').tabs();
@@ -37,11 +71,9 @@ $(document).ready(function() {
 	if(hashtag_id != "None") {
 		$.getJSON("/locations", {}, function(data) {
 			locations = data;
-			PlotOnMap(hashtag_id);
-			// $('#map_path').gmap3();
+			PlotGlobalSpreadOnMap(hashtag_id);
 		});
 	} else {
-		// a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
 		$("#dialog:ui-dialog").dialog("destroy");
 		$("#dialog-message").css('visibility', 'visible');
 		$("#dialog-message").dialog({
@@ -54,18 +86,28 @@ $(document).ready(function() {
 			}
 		});
 	}
+
+	// Init button
+	$("#draw_spread_path_button").button({
+		icons : {
+			primary : "ui-icon-play"
+		},
+	}).click(function() {
+		$('#map_path').gmap3({
+			action : 'clear'
+		});
+		// $('#map_path').gmap3({
+		// action : 'destroy'
+		// });
+		PlotSpreadPathOnMap(null);
+	});
+
 	// 	Init path map
 	$('#map_path').gmap3({
 		action : 'init',
 		options : {
-			center : [57.8, 14.0],
-			zoom : 6
+			zoom : 3
 		},
-		callback : function() {
-			// $('#dropMarkers').click(dropMarkers);
-			// $('#startDance').click(startDance);
-			// $('#stopDance').click(stopDance);
-		}
 	});
 
 });
